e42343e7221e7e82d63f9bc3cd0e6a20381413c3
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-21 23:01:24 +0100

    Updates

---
+++ precache-manifest.2bf38de949c09ba053f35ac107eda709.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c670b961fa67db1335467d530373c6",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "1dead95b3ae238ae7eeb",
+    "url": "/covid-19-tracker/static/css/main.34e4cbb7.chunk.css"
+  },
+  {
+    "revision": "0be41021ab3045ab9868",
+    "url": "/covid-19-tracker/static/js/2.2e8b622d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.2e8b622d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dead95b3ae238ae7eeb",
+    "url": "/covid-19-tracker/static/js/main.3ad62976.chunk.js"
+  },
+  {
+    "revision": "fa0c104a82072024f0ae",
+    "url": "/covid-19-tracker/static/js/runtime-main.5b571555.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/covid.d7265326.png"
+  }
+]);
